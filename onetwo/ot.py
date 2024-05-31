1fc78ec8daf4b7d1782425cff2839edbe1a8a615
Nathan Scales <nkscales@google.com>
2024-05-31 11:20:26 -0700

    Add `HTMLRenderer` to `ot.py`, and sort the lines of `ot.py` for easier maintenance.
    PiperOrigin-RevId: 639094910
    Change-Id: I23eec757e16747cc9aecf22910f0b533c51c493c

--- onetwo/ot.py
+++ onetwo/ot.py
@@ -21,29 +21,31 @@ from onetwo import ot
 
 from onetwo.core import composing
 from onetwo.core import executing
+from onetwo.core import results
 from onetwo.core import routing
 from onetwo.core import sampling
 from onetwo.evaluation import evaluation
 
-evaluate = evaluation.evaluate
 compare_with_critic = evaluation.compare_with_critic
+copy_registry = routing.copy_registry
+evaluate = evaluation.evaluate
+Executable = executing.Executable
+function_call = routing.function_registry
+function_registry = routing.function_registry
+HTMLRenderer = results.HTMLRenderer
+make_composable = composing.make_composable
+make_executable = executing.make_executable
 naive_comparison_critic = evaluation.naive_comparison_critic
 naive_evaluation_critic = evaluation.naive_evaluation_critic
-Executable = executing.Executable
 par_iter = executing.par_iter
 parallel = executing.parallel
-make_executable = executing.make_executable
-repeat = sampling.repeat
-function_registry = routing.function_registry
 RegistryContext = routing.RegistryContext
-function_call = routing.function_registry
-with_current_registry = routing.with_current_registry
-with_registry = routing.with_registry
-copy_registry = routing.copy_registry
-set_registry = routing.set_registry
-make_composable = composing.make_composable
+repeat = sampling.repeat
 run = executing.run
 safe_stream = executing.safe_stream
+set_registry = routing.set_registry
 stream_updates = executing.stream_updates
 stream_with_callback = executing.stream_with_callback
+with_current_registry = routing.with_current_registry
+with_registry = routing.with_registry
 
